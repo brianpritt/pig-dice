8c4133e0abaeb8f7d62f6f6b234cc6b4a3ae2433
Brian Pritt and Ryan Back <ryan@>
2016-11-02 15:17:54 -0700

    does what we want for the most part.

--- js/scripts.js
+++ js/scripts.js
@@ -2,12 +2,18 @@ playerOne = {name: "playerOne", score: 0, tally: 0}
 computer = {name: "computer", score: 0, tally:0}
 var currentPlayer = playerOne;
 
+
+
 function diceRoll() {//rolls dice
   return (1 + Math.floor(Math.random() * 6));
 }
 
 function computerPass() {//how to handle a "pass"
   currentPlayer.score += currentPlayer.tally;
+  $(".computer-roll").text("Computer Rolls: " + computer.tally);
+  $(".p2-score").text(" " + computer.score);
+  $(".roll").text("");
+
   currentPlayer.tally = 0;
 
   if (currentPlayer.name === "playerOne") {
@@ -15,7 +21,6 @@ function computerPass() {//how to handle a "pass"
   } else {
     currentPlayer = playerOne;
   }
-  console.log(currentPlayer.name);
 }
 
 function computerPlay() {//for computer taking its' turn
@@ -40,26 +45,27 @@ function playerTurn() {
     currentPlayer.tally += dice;
   } else {
     currentPlayer.tally = 0;
+    // debugger;
     computerPass();
     computerPlay();
   }
-  return currentPlayer.tally;
+  return dice;
 }
 
 
 
 $(function() {
-
   $(".roll-dice").click(function(){//listens for "roll"
-    $("#roll-score").text("Roll: " + diceRoll());
-    $("#tally-score").text("Tally: " + playerTurn());
+    var turnOfturns = playerTurn();
+    $(".p1-score").text(playerOne.score);
+    $(".p2-score").text(computer.score);
+    $(".roll").text("Roll: " + turnOfturns);
+    $(".tally").text("Tally: " + playerOne.tally);
   });
 
   $(".pass").click(function(){//listens for "pass"
     computerPass();
+    $(".p1-score").text(playerOne.score);
     computerPlay();
   });
-
-
-
 });
